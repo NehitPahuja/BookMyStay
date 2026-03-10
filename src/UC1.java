a4225362a721df7366b9345dd8fe3f205860403c
NehitPahuja <nehitpahuja@gmail.com>
2026-03-10 09:04:20 +0530

    UC1: Add welcome message

---
+++ src/UC1.java
@@ -0,0 +1,11 @@
+public class UC1 {
+
+
+    public static void main(String[] args) {
+
+        //USE CASE 1
+        System.out.println("Welcome to the BookMyStay");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
